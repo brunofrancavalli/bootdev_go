5cda4e0423ed7206c59a3dd0b2bc8e746e17f70d
brunofrancavalli <brunofrancavalli@gmail.com>
2025-10-02 20:59:35 -0500

    test

--- ch5interfaces/l17ProcessNotification/main.go
+++ ch5interfaces/l17ProcessNotification/main.go
@@ -11,10 +11,26 @@ type directMessage struct {
 	isUrgent       bool
 }
 
+func (dM directMessage) importance() int {
+	importance := 0
+
+	if dM.isUrgent {
+		importance = 50
+	} else {
+		importance = dM.priorityLevel
+	}
+
+	return importance
+}
+
 type groupMessage struct {
 	groupName      string
 	messageContent string
 	priorityLevel  int
+}
+
+func (gm groupMessage) importance() int {
+	return gm.priorityLevel
 }
 
 type systemAlert struct {
@@ -22,8 +38,20 @@ type systemAlert struct {
 	messageContent string
 }
 
-// ?
+func (sA systemAlert) importance() int {
+	return 100
+}
 
 func processNotification(n notification) (string, int) {
-	// ?
+	switch v := n.(type) {
+	case directMessage:
+		return v.senderUsername, v.importance()
+	case groupMessage:
+		return v.groupName, v.importance()
+	case systemAlert:
+		return v.alertCode, v.importance()
+	default:
+		return "", 0
+	}
+
 }
