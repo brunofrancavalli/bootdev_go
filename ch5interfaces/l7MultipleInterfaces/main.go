250079474a56974842b6f2f86e0318fc558b3cf3
brunofrancavalli <brunofrancavalli@gmail.com>
2025-10-02 20:06:36 -0500

    got Ch5L17

---
+++ ch5interfaces/l7MultipleInterfaces/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func (e email) cost() int {
+	if e.isSubscribed {
+		return 2 * len(e.body)
+	} else {
+		return 5 * len(e.body)
+	}
+
+}
+
+func (e email) format() string {
+	subscribedText := "Not Subscribed"
+	if e.isSubscribed {
+		subscribedText = "Subscribed"
+	}
+	return fmt.Sprintf("'%s' | %s", e.body, subscribedText)
+}
+
+type expense interface {
+	cost() int
+}
+
+type formatter interface {
+	format() string
+}
+
+type email struct {
+	isSubscribed bool
+	body         string
+}
