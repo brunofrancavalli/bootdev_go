1c704e925d5b1630d827f3612543b752cbd44eee
brunofrancavalli <brunofrancavalli@gmail.com>
2025-10-09 21:23:00 -0500

    got C8L2 completed

--- ch8slices/2slices/main.go
+++ ch8slices/2slices/main.go
@@ -1,10 +1,19 @@
 package main
 
+import "errors"
+
 const (
 	planFree = "free"
 	planPro  = "pro"
 )
 
 func getMessageWithRetriesForPlan(plan string, messages [3]string) ([]string, error) {
-	// ?
+	switch plan {
+	case "free":
+		return messages[0:2], nil
+	case "pro":
+		return messages[:], nil
+	default:
+		return nil, errors.New("unsupported plan")
+	}
 }
