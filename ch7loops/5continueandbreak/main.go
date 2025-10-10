b374eb3aae3a6e5cd0f50887ba23f037e0b83f13
brunofrancavalli <brunofrancavalli@gmail.com>
2025-10-09 20:49:34 -0500

    starting new project

--- ch7loops/5continueandbreak/main.go
+++ ch7loops/5continueandbreak/main.go
@@ -5,7 +5,25 @@ import (
 )
 
 func printPrimes(max int) {
-	// ?
+	for count := 2; count < max; count++ {
+		if count == 2 {
+			fmt.Println(count)
+		} else if count%2 == 0 {
+			continue
+		} else {
+			broke := false
+			for count2 := 2; count2 < count; count2++ {
+				if (count % count2) == 0 {
+					broke = true
+					break
+				}
+			}
+
+			if !broke {
+				fmt.Println(count)
+			}
+		}
+	}
 }
 
 // don't edit below this line
