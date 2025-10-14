2b3b58ad2f3343718036263190960ce22b7831b5
brunofrancavalli <brunofrancavalli@gmail.com>
2025-10-13 22:43:08 -0500

    got C8L6 done

--- ch8slices/6make/main.go
+++ ch8slices/6make/main.go
@@ -1,5 +1,11 @@
 package main
 
 func getMessageCosts(messages []string) []float64 {
-	// ?
+	costSlice := make([]float64, len(messages))
+
+	for index := 0; index < len(messages); index++ {
+		costSlice[index] = float64(len(messages[index])) * float64(0.01)
+	}
+
+	return costSlice
 }
