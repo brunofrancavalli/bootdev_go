75756391a4fb55913c3327c658ecd618a74a9695
Bruno F Valli <brunofrancavalli@gmail.com>
2025-11-14 17:49:45 -0600

    Adding C8L13 information done

--- ch8slices/L13SliceOfSlices/main.go
+++ ch8slices/L13SliceOfSlices/main.go
@@ -1,5 +1,15 @@
 package main
 
 func createMatrix(rows, cols int) [][]int {
-	// ?
+	matrix := [][]int{}
+
+	for row := 0; row < rows; row++ {
+		rowTemp := []int{}
+		for col := 0; col < cols; col++ {
+			rowTemp = append(rowTemp, col*row)
+		}
+		matrix = append(matrix, rowTemp)
+	}
+
+	return matrix
 }
