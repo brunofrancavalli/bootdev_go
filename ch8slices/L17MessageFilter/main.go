27e7c46e5476ddf1c691d6d62f139a61b2532148
brunofrancavalli <brunofrancavalli@gmail.com>
2025-11-16 20:19:37 -0600

    got C8L17 starting

---
+++ ch8slices/L17MessageFilter/main.go
@@ -0,0 +1,49 @@
+package main
+
+type Message interface {
+	Type() string
+}
+
+type TextMessage struct {
+	Sender  string
+	Content string
+}
+
+func (tm TextMessage) Type() string {
+	return "text"
+}
+
+type MediaMessage struct {
+	Sender    string
+	MediaType string
+	Content   string
+}
+
+func (mm MediaMessage) Type() string {
+	return "media"
+}
+
+type LinkMessage struct {
+	Sender  string
+	URL     string
+	Content string
+}
+
+func (lm LinkMessage) Type() string {
+	return "link"
+}
+
+// Don't touch above this line
+
+func filterMessages(messages []Message, filterType string) []Message {
+	filteredMessages := make([]Message, 0)
+
+	for _, message := range messages {
+		messageType := message.Type()
+		if messageType == filterType {
+			filteredMessages = append(filteredMessages, message)
+		}
+	}
+
+	return filteredMessages
+}
