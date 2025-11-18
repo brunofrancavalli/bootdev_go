495645de86ad9a9f228864e2c1262e03912a7d1f
brunofrancavalli <brunofrancavalli@gmail.com>
2025-11-17 20:15:51 -0600

    got C9L2 done

--- ch9maps/L2Mutations/main.go
+++ ch9maps/L2Mutations/main.go
@@ -1,7 +1,19 @@
 package main
 
+import "errors"
+
 func deleteIfNecessary(users map[string]user, name string) (deleted bool, err error) {
-	// ?
+	user, ok := users[name]
+	if ok {
+		if user.scheduledForDeletion {
+			delete(users, name)
+			return true, nil
+		} else {
+			return false, nil
+		}
+	} else {
+		return false, errors.New("not found")
+	}
 }
 
 type user struct {
