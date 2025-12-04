31481fd9763f50e5a01af1b7c323b3429382e89f
Bruno F Valli <brunofrancavalli@gmail.com>
2025-12-04 08:55:00 -0600

    Adding C10L2 started

--- ch10pointers/L2References/main.go
+++ ch10pointers/L2References/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"strings"
+)
+
 func removeProfanity(message *string) {
-	// ?
+	newMessage := message
+	words := []string{"fubb", "shiz", "witch"}
+
+	for _, word := range words {
+
+		*newMessage = strings.Replace(*newMessage, word, getAsterisks(word), -1)
+	}
+}
+
+func getAsterisks(value string) string {
+	asterisks := ""
+
+	for index := 0; index < len(value); index++ {
+		asterisks += "*"
+	}
+
+	return asterisks
 }
