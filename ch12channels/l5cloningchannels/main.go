776c21924fe7bce3cdc8da6b5bf44fbd1246369f
Bruno F Valli <brunofrancavalli@gmail.com>
2026-01-14 12:07:44 -0600

    Done with CH12_L5

---
+++ ch12channels/l5cloningchannels/main.go
@@ -0,0 +1,21 @@
+package main
+
+func countReports(numSentCh chan int) int {
+	totalCount := 0
+
+	for num := range numSentCh {
+		totalCount += num
+	}
+
+	return totalCount
+}
+
+// don't touch below this line
+
+func sendReports(numBatches int, ch chan int) {
+	for i := 0; i < numBatches; i++ {
+		numReports := i*23 + 32%17
+		ch <- numReports
+	}
+	close(ch)
+}
